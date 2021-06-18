8d65a316f708e06f3758e779713ce41a99b42133
ha615 <sdet615@gmail.com>
2021-06-18 07:30:54 +0700

    log4j

--- actions/pageObject/user/nopCommerce/PageGeneratorManager.java
+++ actions/pageObject/user/nopCommerce/PageGeneratorManager.java
@@ -15,58 +15,58 @@ public class PageGeneratorManager {
 	private static DashboardPageObject dashboardPage;
 	
 	public static HomePageObject getHomePape(WebDriver driver) {
-		if(homePage == null) {
-			homePage = new HomePageObject(driver);
-		}
-		return homePage;
+//		if(homePage == null) {
+//			homePage = new HomePageObject(driver);
+//		}
+		return new HomePageObject(driver);
 	}
 	
 	public static RegisterPageObject getRegisterPage(WebDriver driver) {
-		if(registerPage == null) {
-			registerPage = new RegisterPageObject(driver);
-		}
-		return registerPage;
+//		if(registerPage == null) {
+//			registerPage = new RegisterPageObject(driver);
+//		}
+		return new RegisterPageObject(driver);
 	}
 	
 	public static LoginPageObject getLoginPage(WebDriver driver) {
-		if(loginPage == null) {
-			loginPage = new LoginPageObject(driver);
-		}
-		return loginPage;
+//		if(loginPage == null) {
+//			loginPage = new LoginPageObject(driver);
+//		}
+		return new LoginPageObject(driver);
 	}
 	
 	public static SearchPageObject getSearchPage(WebDriver driver) {
-		if(searchPage == null) {
-			searchPage = new SearchPageObject(driver);
-		}
-		return searchPage;
+//		if(searchPage == null) {
+//			searchPage = new SearchPageObject(driver);
+//		}
+		return new SearchPageObject(driver);
 	}
 	
 	public static OrderPageObject getOrderPage(WebDriver driver) {
-		if(orderPage == null) {
-			orderPage = new OrderPageObject(driver);
-		}
-		return orderPage;
+//		if(orderPage == null) {
+//			orderPage = new OrderPageObject(driver);
+//		}
+		return new OrderPageObject(driver);
 	}
 	
 	public static MyAccountPageObject getMyAccountPage(WebDriver driver) {
-		if(myAccountPage == null) {
-			myAccountPage = new MyAccountPageObject(driver);
-		}
-		return myAccountPage;
+//		if(myAccountPage == null) {
+//			myAccountPage = new MyAccountPageObject(driver);
+//		}
+		return new MyAccountPageObject(driver);
 	}
 
 	public static ShoppingCartObject getShoppingCartPage(WebDriver driver) {
-		if(shoppingCart == null) {
-			shoppingCart = new ShoppingCartObject(driver);
-		}
-		return shoppingCart;
+//		if(shoppingCart == null) {
+//			shoppingCart = new ShoppingCartObject(driver);
+//		}
+		return new ShoppingCartObject(driver);
 	}
 
 	public static DashboardPageObject getDashboardPage(WebDriver driver) {
-		if(dashboardPage == null)
-			dashboardPage = new DashboardPageObject(driver);
-		return dashboardPage;
+//		if(dashboardPage == null)
+//			dashboardPage = new DashboardPageObject(driver);
+		return new DashboardPageObject(driver);
 	}
 	
 }
---
+++ testcases/takeScreenshot/TakeScreenshotTestFail.java
@@ -0,0 +1,24 @@
+package takeScreenshot;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class TakeScreenshotTestFail {
+	
+  @Test
+  public void f() {
+	  
+  }
+  
+  @BeforeClass
+  public void beforeClass() {
+	  
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  
+  }
+
+}
